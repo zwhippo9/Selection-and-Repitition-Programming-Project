b4f7a4ba2d665c3d0b057d591a4c0ad407f4a6a1
zwhippo9 <zwhippo9@gmail.com>
2024-09-30 22:58:26 +0000

    final

--- .ccls-cache/@home@runner@Selection-and-Repitition-Programming-Project/main.cpp
+++ .ccls-cache/@home@runner@Selection-and-Repitition-Programming-Project/main.cpp
@@ -10,10 +10,12 @@ void mergeFiles(const string &file1Path, const string &file2Path, const string &
   ofstream output(outputFile);
   
   string line1, line2;
- 
+
+//read first line of both files
   getline (file1, line1);
   getline (file2, line2);
 
+//merging the files
   while(!file1.eof() && !file2.eof()) {
       if (line1 < line2) {
         output << line1 << endl;
@@ -23,6 +25,7 @@ void mergeFiles(const string &file1Path, const string &file2Path, const string &
         getline(file2, line2);
       }
   }
+//for remaining items in file
     while (!file1.eof()) {
         output << line1 << endl;
         getline(file1, line1);
@@ -31,7 +34,7 @@ void mergeFiles(const string &file1Path, const string &file2Path, const string &
         output << line2 << endl;
         getline(file2, line2);
     }
-
+//close files
   file1.close();
   file2.close();
   output.close();
@@ -41,15 +44,16 @@ int main() {
 string file1;
 string file2;
 string output;
+//request file from user
     cout << "Enter the name of the first input file: ";
     cin >> file1;
     cout << "Enter the name of the second input file: ";
     cin >> file2;
     cout << "Enter the name of the output file: ";
     cin >> output;
-  
+//call mergeFiles function
     mergeFiles(file1, file2, output);
-
+//output
     cout  << "Completed " << output << endl;
     return 0;
   }
